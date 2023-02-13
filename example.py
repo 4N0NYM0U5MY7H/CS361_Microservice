f8d42a6c5c24b41609d39cdbca5d7a874c958353
August Frisk <114228717+4N0NYM0U5MY7H@users.noreply.github.com>
2023-02-12 20:51:08 -0800

    Create example.py
    Included a sample client to run with the microservice.

---
+++ example.py
@@ -0,0 +1,38 @@
+# Simple example client to run with the Currency Exchange Microservice
+import time
+import re
+
+# <path_to_requests_file> points to the request file in the microservice directory
+path_to_requests_file = "exchange_rate/requests.txt"
+
+# <path_to_response_file> points to the request file in the microservice directory
+path_to_response_file = "exchange_rate/response.txt"
+
+# example currencies to exchange
+base_currency = "usd"
+target_currency = "eur"
+
+# Send the request to the <path_to_request_file>
+with open(path_to_requests_file, "w") as out_file:
+    out_file.write(f"{base_currency},{target_currency}")
+
+# Poll the <path_to_response_file> until you receive a response
+while True:
+    time.sleep(1)
+    # Receive the response from the <path_to_response_file>
+    with open(path_to_response_file, "r") as in_file:
+        exchange_rate = in_file.readline()
+
+    # No response received
+    if exchange_rate == "":
+        continue
+
+    # Valid reponse received
+    if re.search("^(0|[1-9]\d*)?(\.\d+)?(?<=\d)$", exchange_rate):
+        # acknowledge valid response in <path_to_response_file>
+        with open(path_to_response_file, "w") as out_file:
+            out_file.write("Response Received")
+        break
+
+# View the results
+print(f"The exchange rate from USD to EUR is {exchange_rate}.")
