7ad2e8088ab533b9c9cb0e230d236e90ba32f22a
August Frisk <114228717+4N0NYM0U5MY7H@users.noreply.github.com>
2023-03-13 22:46:51 -0700

    Update exchange_rate.py
    - add customizable variables for path to
      - `data` directory
      - `request` file
      - `response` file
    - cleanup file docstring

--- exchange_rate/exchange_rate.py
+++ exchange_rate/exchange_rate.py
@@ -1,13 +1,26 @@
-# Author: August Frisk
-# GitHub username: @4N0NYM0U5MY7H
-# Date: 2023, February 8
-# Description: This modules constains the functions for the currency exchange
-#              rate microservice.
+r"""rate_X microservice functions.
 
+The exchange_rate module contains all the functions required run the
+rate_X microservice.
 
-r"""This modules contains the functions for the currency exchange rate microservice."""
+Rates by Exchange Rate API: <https://www.exchangerate-api.com>"""
 
-__version__ = "1.0.0"
+__version__ = "1.1.1"
+__author__ = "August Frisk <https://github.com/users/4N0NYM0U5MY7H>"
+
+# --------------------------------------------------------------------
+# internal variables
+
+# Make changes to requests/response file directory here
+_data_directory = "../data"
+
+# Make changes requests/response file names here
+_request_file = "requests.txt"
+_response_file = "response.txt"
+
+# CHANGE WITH CARE
+_path_to_request = f"{_data_directory}/{_request_file}"
+_path_to_response = f"{_data_directory}/{_response_file}"
 
 # --------------------------------------------------------------------
 # public interface
@@ -15,12 +28,12 @@ __version__ = "1.0.0"
 
 def request_path():
     """Returns the file path for the request file."""
-    return "data/requests.txt"
+    return _path_to_request
 
 
 def response_path():
     """Returns the file path for the reponse file."""
-    return "data/response.txt"
+    return _path_to_response
 
 
 def api_url():
